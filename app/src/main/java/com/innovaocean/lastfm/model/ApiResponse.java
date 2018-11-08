f898d6733ddd180ee87eafb122c2843373d2b5a7
Kubra Harmankaya <kubra.harmankaya@fusetech.io>
2018-11-09 01:32:36 +0300

    Initial commit Data Binding and other dependencies added to build.gradle

---
+++ app/src/main/java/com/innovaocean/lastfm/model/ApiResponse.java
@@ -0,0 +1,6 @@
+package com.innovaocean.lastfm.model;
+
+public class ApiResponse<T> {
+
+    public T response;
+}
---
+++ app/src/main/java/com/innovaocean/lastfm/searchList/MainActivity.java
@@ -0,0 +1,20 @@
+package com.innovaocean.lastfm.searchList;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.innovaocean.lastfm.R;
+import com.innovaocean.lastfm.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+}
